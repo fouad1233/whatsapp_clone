05bf6c1a207d53e05f9137a3e57ee7cf0bd4290a
Fouad Aladhami <fouadiadhami@gmail.com>
2023-02-05 00:23:10 +0300

    DefaultTabController is added default tab bar is maked

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("This is the mobile screen layout"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "WhatsApp",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              color: Colors.grey,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+              color: Colors.grey,
+            ),
+          ],
+          bottom: const TabBar(
+            //indicator color is the color of the line under the tab
+            indicatorColor: tabColor,
+            //indicatorWeight is the thickness of the line under the tab
+            indicatorWeight: 4,
+            
+            tabs: [
+              // Tab(
+              //   icon: Icon(Icons.camera_alt),
+              // ),
+              Tab(
+                text: "CHATS",
+              ),
+              Tab(
+                text: "STATUS",
+              ),
+              Tab(
+                text: "CALLS",
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
